db98603ec525140dba1ea3f7a8807fff4d9527db
dabealu <dabealu@gmail.com>
2018-03-09 16:58:54 +0300

    use hyperkube, bump k8s to 1.8.8

--- roles/kubernetes/common/files/component-version.sh
+++ roles/kubernetes/common/files/component-version.sh
@@ -1,15 +1,20 @@
 #!/bin/bash -e
 
-if [[ "$1" == "kubectl" ]]; then
-  kubectl version 2>/dev/null | grep 'Client Version' | sed 's/.*GitVersion:"v\([0-9]*\.[0-9]*\.[0-9]*\)".*/\1/'
-
-elif [[ "$1" == "etcd" ]]; then
-  etcdctl --version | grep 'etcdctl' | sed 's/etcdctl version: \(.*\)/\1/'
-
-elif [[ "$1" =~ kube-apiserver|kube-scheduler|kube-controller-manager|kubelet|kube-proxy ]]; then
-  $1 --version 2>/dev/null | cut -f 2 -d 'v'
-
-else
-  echo 'error: first arg must be the name of k8s component, i.e. kube-apiserver'
-  exit 1
-fi
+case $1 in
+  "kubernetes")
+    hyperkube --version | cut -f 2 -d 'v'
+    ;;
+  "etcd")
+    etcdctl --version | grep 'etcdctl' | sed 's/etcdctl version: \(.*\)/\1/'
+    ;;
+  "calicoctl")
+    calicoctl version | grep 'Client Version:' | cut -f 2 -d 'v'
+    ;; 
+  "cfssl")
+    cfssl version | grep 'Version:' | sed 's/Version: \([0-9]*\.[0-9]*\).*/\1/'
+    ;;
+  *)
+    echo "error: first arg must be one of: kubernetes, etcd, calicoctl, cfssl"
+    exit 1
+    ;;
+esac
